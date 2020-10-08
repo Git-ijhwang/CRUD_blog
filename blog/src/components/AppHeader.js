fa00d4be281029425a89402c0b9ac09e14508025
Git-ijhwang <hwangij@gmail.com>
2020-10-08 19:11:09 -0300

    blog initial page

---
+++ blog/src/components/AppHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+const AppHeader = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" color="inherit">
+        My React Blog
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+
+export default AppHeader;
